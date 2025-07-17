b47bd2599f8cce1a2355603ac55f7f49e97ee7e3
simdarya <darasimasko@gmail.com>
2025-07-18 02:56:46 +0300

    Delete kittygram_final directory

--- kittygram_final/frontend/src/components/ui/form-container/form-container.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./form-container.module.css";
-
-export const FormContainer = ({ children, extraClass = "" }) => {
-  return <div className={`${styles.content} ${extraClass}`}>{children}</div>;
-};
